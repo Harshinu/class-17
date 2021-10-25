e48b593f1dc446050e6e0dd9703257a4423209a3
Harshinu <90198903+Harshinu@users.noreply.github.com>
2021-10-25 19:51:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -214,28 +214,17 @@ function redCyclists(){
 //  distance = 0;
 // }
 
-//function reset(){
-//  gameState = PLAY;
-//  gameOver.visible = false;
-//  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
+function reset(){
+ gameState = PLAY;
+ gameOver.visible = false;
+ mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
   
-//  pinkCG.destroyEach();
-//  yellowCG.destroyEach();
-//  redCG.destroyEach();
+ pinkCG.destroyEach();
+ yellowCG.destroyEach();
+ redCG.destroyEach();
   
-//  distance = 0;
-// }
+ distance = 0;
+}
 
-//function reset(){
-//  gameState = END;
-//  gameOver.visible = true;
-//  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
-  
-//  pinkCG.destroyEach();
-//  yellowCG.destroyEach();
-//  redCG.destroyEach();
-  
-//  distance = 50;
-// }
 
 
